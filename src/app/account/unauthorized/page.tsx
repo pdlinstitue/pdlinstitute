5dce6936b62cc3c59972ce4e74cd509d7be52680
PDL Institute <mail2pdlinstitute@gmail.com>
2025-06-03 08:29:16 +0530

    un

--- src/app/account/unauthorized/page.tsx
+++ src/app/account/unauthorized/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const UnauthorizedAccess = () => {
 
   return (
-    <div>
+    <div className='flex items-center justify-center py-40'>
       <div className='formStyle w-[400px]'>
         <h1 className='text-2xl text-red-600'>Unauthorized Access</h1>
         <p>Opps! You are unauthorized to use this resourse.</p>
