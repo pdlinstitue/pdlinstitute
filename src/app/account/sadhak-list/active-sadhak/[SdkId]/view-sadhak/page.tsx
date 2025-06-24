6f3e754dd75cb3c3e4185f0192172e8ee0245b24
dilipsingh92 <84071801+dilipsingh92@users.noreply.github.com>
2025-06-24 17:50:26 +0530

    view fixes

--- src/app/account/sadhak-list/active-sadhak/[SdkId]/view-sadhak/page.tsx
+++ src/app/account/sadhak-list/active-sadhak/[SdkId]/view-sadhak/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ViewSadhak from "./ViewSadhak";
 import { BASE_API_URL } from "@/app/utils/constant";
+import { cookies } from "next/headers";
 
 interface ISadhakParams {
   params: Promise<{
@@ -8,34 +9,38 @@ interface ISadhakParams {
   }>;
 }
 
-const ViewSadhakById: React.FC<ISadhakParams> = async ({ params }: ISadhakParams) => {
-
+const ViewSadhakById: React.FC<ISadhakParams> = async ({
+  params,
+}: ISadhakParams) => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
   const { SdkId } = await params;
-  let sdkData = null;
+  let sdkDataById = null;
 
   try {
     const res = await fetch(`${BASE_API_URL}/api/users/${SdkId}/view-sadhak`, {
+      method: "GET",
       cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken}`, // ✅ manually pass cookie
+      },
     });
 
     if (!res.ok) {
       throw new Error("Failed to fetch sadhak data");
     }
-    
-    const sadhakData  = await res.json();
-    sdkData = sadhakData.sdkById;
 
+    const sadhakData = await res.json();
+    sdkDataById = sadhakData.sdkById;
   } catch (error) {
     console.error("Error fetching sadhak data:", error);
   }
 
-  if (!sdkData) {
-    return <div>Failed to load sadhak data.</div>;
-  }
-
   return (
     <div>
-      <ViewSadhak sdkData={sdkData} />
+      <ViewSadhak sdkData={sdkDataById} />
     </div>
   );
 };
